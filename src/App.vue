c3c5eee1b365537f902df5fa34039f60e54f56e0
itsjackie00 <jackiedanso33@gmail.com>
2024-06-18 16:22:07 +0200

    try

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
-
 <template>
-  <h1>Ciao</h1>
-  <i class="fa-solid fa-house"></i>
+  <div id="app">
+    <h1>Lista dei Progetti</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-  }
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  async created() {
+    try {
+      // Fai una chiamata API all'endpoint Laravel per ottenere i progetti
+      const response = await axios.get('http://localhost:8000/api/projects');
+      // Salva i dati dei progetti nella variabile `projects`
+      this.projects = response.data;
+      // Stampa i dati dei progetti in console per verificarli
+      console.log(response.data);
+    } catch (error) {
+      console.error('Errore nel recupero dei progetti:', error);
+    }
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+/* Puoi aggiungere il tuo stile CSS qui */
 </style>
